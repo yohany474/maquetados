86a17b7e090a0b3e350c2fe44d6088588eefae2a
Yohany <109817527+yohany474@users.noreply.github.com>
2023-09-05 22:57:58 -0500

    Add files via upload

---
+++ motora/js/incrementar.js
@@ -0,0 +1,13 @@
+
+function incrementarpositivo() {
+    var j = document.getElementById('boton-sumar')
+    j = 1;
+    for(var i; j=1; j++ )
+    document.result.num.value=(++j);
+}
+function incrementarnegativo() {
+    var j = document.getElementById('boton-restar')
+    var valor ;
+    j = 1;
+    document.result.num.value-=1;
+}
